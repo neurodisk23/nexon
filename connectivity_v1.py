c3771bbd362b020a08e8316c2d40e6dc770e028c
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-07-22 16:50:18 +0530

    Create connectivity_v1.py

---
+++ connectivity_v1.py
@@ -0,0 +1,73 @@
+import socket
+import time
+
+def send_data(ip_address, port, message):
+    try:
+        # Create a TCP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Connect to the IP address and port
+        sock.connect((ip_address, port))
+        token  = 0
+        while token < 25:
+            # Send the message
+            data = sock.send(message.encode())
+            print(data)
+            token += data 
+            
+            # Delay for 1 second
+            time.sleep(1)
+        
+    except Exception as e:
+        print('An error occurred while sending data:', str(e))
+    
+    finally:
+        # Close the socket
+        sock.close()
+
+# Example usage
+ip_address = '169.254.178.227'
+port = 5001
+message = "A1,N,0,0,0,0,0,0,0,0,0\r\n"
+
+# Call the send_data function
+send_data(ip_address, port, message)
+
+message = "A1,N,0,0,0,0,0,0,0,0,0\r\n"
+
+# Call the send_data function
+send_data(ip_address, port, message)
+
+message = "A,N,0,1,50,0,0,0,0,0,0\r\n"
+
+send_data(ip_address, port, message)
+
+message = "A,D,0,1,50,0,0,0,0,1,0\r\n"
+
+send_data(ip_address, port, message)
+
+message = "A,D,20,0,0,0,0,0,1,0,0\r\n"
+
+send_data(ip_address, port, message)
+message = "A,D,20,0,0,0,0,1,0,0,0\r\n"
+
+send_data(ip_address, port, message)
+
+message = "A,N,0,0,50,0,0,0,0,0,0\r\n"
+
+send_data(ip_address, port, message)
+message = "A,N,0,0,50,0,0,0,0,0,0\r\n"
+
+send_data(ip_address, port, message)
+
+message = "A,N,0,0,0,0,0,0,0,0,0\r\n"
+
+send_data(ip_address, port, message)
+message = "A,N,0,0,0,0,0,0,0,0,0\r\n"
+
+send_data(ip_address, port, message)
+
+message = "M,N,0,0,0,0,0,0,0,0,0\r\n"
+
+# Call the send_data function
+send_data(ip_address, port, message)
